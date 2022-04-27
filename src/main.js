cc35d699455ad0860adb6ca43002a5fa68abde39
chenzh <515740090@qq.com>
2022-04-28 00:02:00 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,90 @@
+const $siteList = $('.siteList');
+const $lastLi = $siteList.find('li.last');
+const x = localStorage.getItem('x');
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+    { logo: 'A', url: 'https://www.acfun.cn' },
+    { logo: 'B', url: 'https://www.bilibili.com' }
+]
+const simplifyUrl = (url) => {
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '')
+}
+const render = () => {
+    $siteList.find('li:not(.last)').remove();
+    hashMap.forEach((node, index) => {
+        const $li = $(`<li>
+                <div class="site">
+                    <div class="logo">${node.logo}</div>
+                    <div class="link">${simplifyUrl(node.url)}</div>
+                    <div class="close">
+                    <svg class="icon">
+                        <use xlink:href="#icon-close"></use>
+                    </svg>
+                    </div>
+                </div>
+        </li>`).insertBefore($lastLi)
+        $li.on('click', () => {
+            window.open(node.url, "_parent")
+        })
+        $li.on('click', '.close', (e) => {
+            e.stopPropagation()//阻止冒泡
+            hashMap.splice(index, 1)
+            render()
+        })
+    });
+}
+
+render();
+$('.addButton')
+    .on('click', () => {
+        let url = window.prompt('请输入添加的网址:')
+        if (url.indexOf('http') !== 0) {
+            url = 'https://' + url
+        }
+        hashMap.push(
+            {
+                logo: simplifyUrl(url)[0].toUpperCase(),
+                logoType: "text",
+                url: url
+            }
+        )
+        render();
+    });
+window.onbeforeunload = () => {
+    console.log('页面要关闭了');
+    const string = JSON.stringify(hashMap);
+    localStorage.setItem('x', string);
+}
+
+//键盘事件
+$(document).on('keypress', (e) => {
+    const { key } = e   //const key =e.key
+    for (let i = 0; i < hashMap.length; i++) {
+        if (hashMap[i].logo.toLowerCase() === key) {
+            window.open(hashMap[i].url)
+        }
+    }
+})
+
+// $('.addButton')
+//     .on('click', () => {
+//         let url = window.prompt('请输入添加的网址:')
+//         if (url.indexOf('http') !== 0) {
+//             url = 'https://' + url
+//         }
+//         console.log(url)
+//         const $siteList = $('.siteList')
+//         const $lastLi = $siteList.find('li.last')
+//         const $li = $(`<li>
+//         <a href="${url}">
+//             <div class="site">
+//                 <div class="logo">${url[8]}</div>
+//                 <div class="link">${url}</div>
+//             </div>
+//         </a>
+//     </li>`).insertBefore($lastLi)
+//     })
+
